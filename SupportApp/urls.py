7fdbf126a57a37e527d36176821580ff4667682b
yaser <ydahgan@gmail.com>
2023-11-14 07:47:15 +0330

    Change Name of Project To SupportApp

--- SupportApp/urls.py
+++ SupportApp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Ticket project.
+URL configuration for SupportApp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls"))
+    path("accounts/", include("accounts.urls")),
 
 ]

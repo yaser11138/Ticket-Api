586a7c4291a620495f0009584112b4d0ecf1e062
yaser <ydahgan@gmail.com>
2023-11-15 12:02:14 +0330

    Add Ticket View

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OpenDiscussion
+from .views import OpenDiscussion,CreateTask
 
 urlpatterns = [
     path("open-discussion/", OpenDiscussion.as_view(), name="open-discussion"),
+    path("create-ticket/", CreateTask.as_view(), name="create-ticket"),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CombinedDiscussionTicketSerializer
+from .serializers import CombinedDiscussionTicketSerializer, TicketSerializer
+from .models import Discussion
 
 
 class OpenDiscussion(APIView):
@@ -19,3 +20,18 @@ class OpenDiscussion(APIView):
             return Response(data={"status": "Discussion Opened", "data": combined_serializer.data}, status=status.HTTP_201_CREATED)
         else:
             return Response(data=combined_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateTask(APIView):
+
+    def post(self, request, discussion_id):
+        discussion = Discussion.objects.get(id=discussion_id)
+        ticket_serializer = TicketSerializer(data=request.data)
+        if ticket_serializer.is_valid():
+            ticket_serializer.validated_data["discussion"] = discussion
+            ticket_serializer.validated_data["user"] = user
+            ticket_serializer.save()
+            return Response(data={"ticket": ticket_serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data={"error": ticket_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+

10eb7a2b8a5a85ee2a241691a06b9574026d1922
mohammad foroutan <mmdfrn81@gmail.com>
2023-12-28 15:15:28 +0330

    change db engine to mysql

--- SupportApp/settings.py
+++ SupportApp/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "SupportApp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.mysql",
         "NAME": env("name"),
         "USER": env("user"),
         "PASSWORD": env("password"),

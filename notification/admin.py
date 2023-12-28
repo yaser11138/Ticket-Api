aa911b7b5d144bb5c81ff1f3abf1e9dc16043b37
yaser <ydahgan@gmail.com>
2023-12-28 18:23:56 +0330

    remove ntification

--- notification/admin.py
+++ notification/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Notification
-# Register your models here.
-
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    pass
--- notification/utils.py
+++
@@ -1,9 +0,0 @@
-from .models import Notification
-
-
-def create_notification(user, action, content_object):
-    try:
-        Notification.objects.create(user=user, action=action, content_object=content_object)
-        return True
-    except ValueError:
-        return False

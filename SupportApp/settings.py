968aa88d5e6e4243d08a74a2a58c4996217540cb
mohammad foroutan <mmdfrn81@gmail.com>
2023-12-29 12:05:05 +0330

    add production and devevelopment environment

--- SupportApp/settings.py
+++ SupportApp/settings.py
@@ -28,7 +28,7 @@ env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if env('py_env') == 'development' else False
 
 ALLOWED_HOSTS = ["gglink.ir", "www.gglink.ir"]
 

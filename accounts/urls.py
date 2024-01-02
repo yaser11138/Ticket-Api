b5c911049a0ffd37a5da307eb1cc1068b95a4127
yaser <ydahgan@gmail.com>
2024-01-02 10:08:43 +0330

    add user view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView
+from .views import RegisterView, UserView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="user-register"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("user/<int:id>/", UserView.as_view(), name="user"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework import status
-from .serializers import UserRegisterSerializer
+from rest_framework.pagination import PageNumberPagination
+from .serializers import UserRegisterSerializer, UserSerializer
+
+User = get_user_model()
 
 
 class RegisterView(APIView):
@@ -17,6 +21,27 @@ class RegisterView(APIView):
         else:
             return Response(data=user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserView(APIView, PageNumberPagination):
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        user = request.user
+        if user:
+            user_serializer = UserSerializer(instance=user)
+            user_data = user_serializer.data
+            return Response(data=user_data, status=status.HTTP_200_OK)
+        else:
+            data = {"error": "user not found"}
+            return Response(data=data, status=status.HTTP_400_BAD)
+
+    def list(self, request):
+        users = User.objects.all()
+        user_of_this_page = self.paginate_queryset(users, request)
+        user_serializer = self.serializer(instance=user_of_this_page, many=True)
+        user_data = user_serializer.data
+        user_paginated_data = self.get_paginated_response(user_data)
+        return Response(data=user_paginated_data, status=status.HTTP_200_OK)
 
 
 

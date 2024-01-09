36388b2e9b6421df5a6629f7bf49b03579707f09
yaser <ydahgan@gmail.com>
2024-01-09 09:46:47 +0330

    improve swagger schema

--- ticket/views.py
+++ ticket/views.py
@@ -1,42 +1,132 @@
 from django.db import transaction
 from rest_framework.views import APIView
+from rest_framework.serializers import ChoiceField
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.pagination import PageNumberPagination
-from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter, OpenApiExample
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+    OpenApiParameter,
+    OpenApiExample,
+    inline_serializer,
+    OpenApiResponse,
+    OpenApiExample,
+)
 from drf_spectacular.types import OpenApiTypes
-from .serializers import CombinedDiscussionTicketSerializer, TicketSerializer, DiscussionSerializer,\
-    DiscussionListSerializer
+from .serializers import (
+    CombinedDiscussionTicketSerializer,
+    TicketSerializer,
+    DiscussionSerializer,
+    DiscussionListSerializer,
+)
 from .models import Discussion
 from .permissions import IsnotStaff, IsOwnerOrStuff
 
 
 @extend_schema_view(
-    list=extend_schema(description="if user is a staff member it gives all of discussions"
-                                   "and if is not i just give use discussion"),
-    create=extend_schema(description="Create a new discussion"),
+    list=extend_schema(
+        description="if user is a staff member it gives all of discussions"
+        "and if is not i just give use discussion",
+        summary="list of users",
+    ),
+    create=extend_schema(
+        description="You can Crate a New Discussion with the first ticket of the discussion be aware that only non "
+                    "staff user can create discussion",
+        summary="Create a new discussion",
+    ),
     retrieve=extend_schema(
-        description="Retrieve details of a discussion by ID",
-        parameters=[OpenApiParameter('id', int, OpenApiParameter.PATH)]
+        description="You can Retrieve details of a discussion by ID and get all of it information",
+        parameters=[OpenApiParameter("id", int, OpenApiParameter.PATH)],
+        summary="Retrieve a discussion",
     ),
     rate=extend_schema(
-        description="Rate a discussion",
-        parameters=[OpenApiParameter('id', int, OpenApiParameter.PATH)],
-        request=CombinedDiscussionTicketSerializer,
-        responses={status.HTTP_200_OK: {"detail": "The discussion rated"}}
+        description="you can rate the discussion specified by id by sending rate number",
+        summary="Rate discussion",
+        parameters=[OpenApiParameter("id", int, OpenApiParameter.PATH)],
+        request=inline_serializer(
+            "DiscussionRateSerializer",
+            {
+                "rate": ChoiceField(
+                    choices=[
+                        ("1", "Poor"),
+                        ("2", "Fair"),
+                        ("3", "Good"),
+                        ("4", "Very Good"),
+                        ("5", "Perfect"),
+                    ],
+                    required=True,
+                )
+            },
+        ),
+        responses={
+            200: OpenApiResponse(
+                OpenApiTypes.OBJECT,
+                "success",
+                [
+                    OpenApiExample(
+                        name="discussion rate successfuly example",
+                        value={"detail": "The discussion rated"},
+                    )
+                ],
+            ),
+            400: OpenApiResponse(
+                OpenApiTypes.OBJECT,
+                "bad request",
+                [
+                    OpenApiExample(
+                        name="discussion rate not provided example",
+                        value={"detail": "Rate hasn't been sent"},
+                    )
+                ],
+            ),
+            404: OpenApiResponse(
+                OpenApiTypes.OBJECT,
+                "not found discussion",
+                [
+                    OpenApiExample(
+                        name="discussion not found example",
+                        value={"detail": "Discussion not found"},
+                    )
+                ],
+            ),
+        },
     ),
     close=extend_schema(
-        description="Close a discussion",
-        parameters=[OpenApiParameter('id', int, OpenApiParameter.PATH)],
-        responses={status.HTTP_200_OK: {"detail": "The discussion closed"}}
+        description="You can Close a discussion by ID you don't have to send anything in request body",
+        summary="close a discussion",
+        parameters=[OpenApiParameter("id", int, OpenApiParameter.PATH)],
+        request=None,
+        responses={
+            200: OpenApiResponse(
+                OpenApiTypes.OBJECT,
+                "success",
+                [
+                    OpenApiExample(
+                        name="discussion closed successfuly example",
+                        value={"detail": "The discussion closed"},
+                    )
+                ],
+            ),
+            404: OpenApiResponse(
+                OpenApiTypes.OBJECT,
+                "not found discussion",
+                [
+                    OpenApiExample(
+                        name="discussion not found example",
+                        value={"detail": "Discussion not found"},
+                    )
+                ],
+            ),
+        },
     ),
 )
 class DiscussionTicketViewSet(viewsets.ViewSet, PageNumberPagination):
     def get_permissions(self):
-        if self.action == 'create' or self.action == 'rate':
+        if self.action == "create" or self.action == "rate":
             permission_classes = [IsAuthenticated, IsnotStaff]
         elif self.action == "retrive":
             permission_classes = [IsAuthenticated, IsOwnerOrStuff]
@@ -52,16 +142,19 @@ class DiscussionTicketViewSet(viewsets.ViewSet, PageNumberPagination):
 
     @transaction.atomic()
     def create(self, request, *args, **kwargs):
-        combined_serializer = self.get_serializer(data=request.data)
+        combined_serializer = self.serializer_class(data=request.data)
         combined_serializer.is_valid(raise_exception=True)
 
         user = request.user
-        combined_serializer.validated_data['user'] = user
+        combined_serializer.validated_data["user"] = user
 
         self.perform_create(combined_serializer)
 
-        data = combined_serializer.data['discussion']
-        return Response(data={"status": "Discussion Opened", "data": data}, status=status.HTTP_201_CREATED)
+        data = combined_serializer.data["discussion"]
+        return Response(
+            data={"status": "Discussion Opened", "data": data},
+            status=status.HTTP_201_CREATED,
+        )
 
     def list(self, request):
         if request.user.is_staff:
@@ -76,32 +169,48 @@ class DiscussionTicketViewSet(viewsets.ViewSet, PageNumberPagination):
         try:
             discussion = Discussion.objects.get(pk=pk)
         except Discussion.DoesNotExist:
-            return Response(data={"detail": "Discussion not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                data={"detail": "Discussion not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
         serializer = DiscussionSerializer(discussion)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["put"])
     def rate(self, request, pk=None):
-        if 'rate' not in request.data:
-            return Response(data={"error": "Rate hasn't been sent"}, status=status.HTTP_400_BAD_REQUEST)
+        if "rate" not in request.data:
+            return Response(
+                data={"error": "Rate hasn't been sent"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         try:
             discussion = Discussion.objects.get(pk=pk)
         except Discussion.DoesNotExist:
-            return Response(data={"detail": "Discussion not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                data={"detail": "Discussion not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
         discussion.rate = request.data.get("rate")
         discussion.save()
-        return Response(data={"detail": "The discussion rated"}, status=status.HTTP_200_OK)
+        return Response(
+            data={"detail": "The discussion rated"}, status=status.HTTP_200_OK
+        )
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["put"])
     def close(self, request, pk=None):
         try:
             discussion = Discussion.objects.get(pk=pk)
         except Discussion.DoesNotExist:
-            return Response(data={"detail": "Discussion not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                data={"detail": "Discussion not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
         discussion.is_terminated = True
         discussion.save()
-        return Response(data={"detail": "The discussion closed"}, status=status.HTTP_200_OK)
+        return Response(
+            data={"detail": "The discussion closed"}, status=status.HTTP_200_OK
+        )
 
 
 class CreateTicket(APIView):
@@ -111,7 +220,7 @@ class CreateTicket(APIView):
     @extend_schema(
         request=TicketSerializer,
         description="Create a new ticket for for the discussion by ID",
-        parameters=[OpenApiParameter('discussion_id', int, OpenApiParameter.PATH)],
+        parameters=[OpenApiParameter("discussion_id", int, OpenApiParameter.PATH)],
     )
     def post(self, request, discussion_id):
         discussion = Discussion.objects.get(id=discussion_id)
@@ -121,6 +230,11 @@ class CreateTicket(APIView):
             ticket_serializer.validated_data["user"] = request.user
             ticket_serializer.save()
             discussion.set_is_answered_vlaue(request.user)
-            return Response(data={"ticket": ticket_serializer.data}, status=status.HTTP_201_CREATED)
+            return Response(
+                data={"ticket": ticket_serializer.data}, status=status.HTTP_201_CREATED
+            )
         else:
-            return Response(data={"error": ticket_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                data={"error": ticket_serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )

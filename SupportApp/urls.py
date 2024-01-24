fa11743bf7b54fc10d1b56fe6cf10d0d8e92aa35
yaser <ydahgan@gmail.com>
2024-01-24 10:00:13 +0330

    remove unnesscary urls

--- SupportApp/urls.py
+++ SupportApp/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("ssacc/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("support/", include("ticket.urls")),

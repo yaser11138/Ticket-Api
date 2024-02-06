91e0a2294bdb4a8cae671b7c39c8b8ad42da2428
mohammad foroutan <mmdfrn81@gmail.com>
2024-02-06 05:48:55 +0000

    change pagination page number to 100

--- SupportApp/settings.py
+++ SupportApp/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
